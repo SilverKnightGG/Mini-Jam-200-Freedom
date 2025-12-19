fe1d964005b4451ac1b841fe72d1b108558efaee
Keith Winget <keith2blu@gmail.com>
2025-12-19 08:34:16 -0500

    Refactored extractable resources to just be a enum in Town -Now everything is just handled in that class, since it always  cares about these things in the same ways
    Signed-off-by: Keith Winget <keith2blu@gmail.com>

--- Data/extractable_resource.gd
+++
@@ -1,9 +0,0 @@
-class_name ExtractableResource extends Resource
-
-
-
-enum Type {FUEL, FOOD, MATERIALS, LUXURY}
-@export var name: String = ""
-@export var type: Type = Type.FUEL
-@export_range(1.0, 200.0, 1.0, "suffix:1 to 200") var tax_value = 1.0
-@export_range(0.0, 20.0, 1.0, "suffix:0 to 20") var town_value = 1.0
--- Main Scenes/town.gd
+++ Main Scenes/town.gd
@@ -1,11 +1,25 @@
 class_name Town extends Node
 
+enum ExtractableResource {
+	## Resource used based on town size to heat the town seasonally
+	FUEL,
+	## Resource used at all times by the town, and required for expansion
+	FOOD,
+	## Resources not used by the town, but brings a high value for paying taxes.
+	MATERIALS,
+	## Resource used exclusively for expanding the town, and fetches a decent value for paying taxes
+	LUXURY}
 
-## Resource used based on town size to heat the town seasonally
-var fuel: float = 0
-## Resource used at all times by the town, and required for expansion
-var food: float = 0
-## Resources not used by the town, but brings a high value for paying taxes
-var luxury: float = 0
-## Resource used exclusively for expanding the town, and fetches a decent value for paying taxes
-var materials: float = 0
+const TAX_VALUE_MUPLTIPLIERS: Dictionary = {
+	ExtractableResource.FUEL: 2.0,
+	ExtractableResource.FOOD: 1.0,
+	ExtractableResource.MATERIALS: 5.0,
+	ExtractableResource.LUXURY: 25.0
+}
+
+var quantities: Dictionary[ExtractableResource, float] = {
+	ExtractableResource.FUEL: 0.0,
+	ExtractableResource.FOOD: 0.0,
+	ExtractableResource.MATERIALS: 0.0,
+	ExtractableResource.LUXURY: 0.0
+}
